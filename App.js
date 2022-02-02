7a92cde72ca89fc8c78df32396ebb964e954602b
khaledlas <khaledlas@gmail.com>
2022-02-02 17:41:00 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import NavbarC from "./Components/Navbar/Navbar"
+import Userlist from './Components/Userlist/Userlist';
+const App = () => {
+  return (
+    <div>
+      <NavbarC/>
+      <Userlist/>
+    </div>
+  );
+}
+
+export default App;
